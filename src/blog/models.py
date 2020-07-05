ed9d8660fc0a2e4f4b6d78deab371230b4166816
Victor Vilela <vilela_victor@hotmail.com>
2020-07-04 20:05:20 -0500

    add models

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+from organizer.models import StartUp, Tag
+
+class Post(models.Model):
+    title = models.CharField(max_length=63)
+    slug = models.SlugField(max_length=63)
+    text = models.TextField()
+    pub_date = models.DateField()
+    tags = models.ManyToManyField(Tag)
+    startups = models.ManyToManyField(StartUp)
--- src/organizer/models.py
+++ src/organizer/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+class Tag(models.Model):
+    name = models.CharField(
+        max_legth=31,
+        unique=True
+    )
+    slug = models.SlugField(
+        max_legth=31,
+        unique=True,
+        help_text="A label for URL config"
+    )
+
+
+class StartUp(models.Model):
+    name = models.CharField(
+        max_length=31,
+        db_index=True
+    )
+    slug = models.SlugField(
+        max_length=31,
+        unique=True,
+        help_text="A label for URL config"
+    )
+    description = models.TextField()
+    founded_date = models.DateField("date founded")
+    contact = models.EmailField()
+    website = models.URLField(max_length=255)
+    tags = models.ManyToManyField(Tag)
+
+
+class NewsLink(models.Model):
+    title = models.CharField(max_length=31)
+    slug = models.SlugField(max_length=31)
+    pub_date = models.DateField("date published")
+    link = models.URLField(max_length=255)
+    startup = models.ForeignKey(
+        StartUp, on_delete=models.CASCADE
+    )
